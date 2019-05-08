b65d7d5f6ab71cbe0dd20dfa6ed512baefcc5f6f
filip <filip.christofferson@gmail.com>
2019-05-08 14:54:22 +0200

    Do css stuff to make it easier to read

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -1,14 +1,20 @@
 <template>
-    <div>
-        <p>
+    <div id="gameContainer">
+        <header id="header">
+            TEMP BEFORE ROUTING WORKS
+        </header>
+        <p id="question">
             {{this.$store.state.questions[questionNumber].question}}
         </p><br>
-        <p>
-            Highest Answer: {{this.$store.state.highAnswers[0]}} Lowest Answer: {{this.$store.state.lowAnswers[0]}}
+        <p id="hiTracker">
+            Highest Answer: {{this.$store.state.highAnswers[0]}}
         </p>
-        
-        <input v-model="answer" name='answer' placeholder="Enter your answer">
-        <button @click="enterAnswer">Submit answer</button>
+        <p id="loTracker">
+            Lowest Answer: {{this.$store.state.lowAnswers[0]}}
+        </p>
+
+        <input v-model="answer" name='answer' placeholder="Enter your answer" id="answer">
+        <button @click="enterAnswer" id="submit">Submit answer</button>
     </div>
 </template>
 
@@ -26,31 +32,123 @@
                 return this.$store.getters.getQuestions;
             },
             enterAnswer: function () {
+
                 if (this.answer < this.$store.state.questions[this.questionNumber].answer) {
                     console.log('This answer is to low');
                     this.$store.state.lowAnswers.push(this.answer);
+                    this.$store.state.answers.push(this.answer);
                     this.$store.state.lowAnswers.reverse();
                     console.log(this.$store.state.lowAnswers);
                 }
                 if (this.answer > this.$store.state.questions[this.questionNumber].answer) {
-                    console.log('This answer is to high')
+                    console.log('This answer is to high');
                     this.$store.state.highAnswers.push(this.answer);
+                    this.$store.state.answers.push(this.answer);
                     this.$store.state.highAnswers.sort();
                     console.log(this.$store.state.highAnswers);
                 }
                 if (this.answer == this.$store.state.questions[this.questionNumber].answer) {
                     console.log('Answer is correct');
+                    this.$store.state.answers.push(this.answer);
+                    console.log(this.$store.state.answers);
                     this.questionNumber += 1;
                     this.$store.state.highAnswers = [];
                     this.$store.state.lowAnswers = [];
                 }
-                console.log(this.answer)
-                //this.$emit('answerQuestion', newAnswer)
+                this.answer = '';
             }
         }
     }
 </script>
 
 <style scoped>
+    @media only screen and (min-width: 900px) {
+        #gameContainer {
+            display: grid;
+            grid-template-columns: repeat(6, 1fr);
+            grid-template-rows: repeat(5, 1fr);
+            background-color: aqua
+        }
 
+        #header {
+            background-color: gray;
+            grid-column: 1 / span 6;
+            grid-row: 1;
+            text-align: center;
+        }
+
+        #question {
+            grid-row: 2;
+            grid-column: 3 / span 2;
+            border: solid black 1px;
+        }
+
+        #answer {
+            grid-row: 4;
+            grid-column: 3 / span 2;
+        }
+
+        #submit {
+            grid-row: 5;
+            grid-column: 3 / span 2;
+
+        }
+
+        #hiTracker {
+            grid-row: 3;
+            grid-column: 3;
+            border: solid black 1px;
+        }
+
+        #loTracker {
+            grid-row: 3;
+            grid-column: 4;
+            border: solid black 1px;
+        }
+    }
+
+    @media only screen and (max-width: 899px) {
+        #gameContainer {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            grid-template-rows: repeat(5, 1fr);
+            background-color: aqua
+        }
+
+        #header {
+            background-color: gray;
+            grid-column: 1 / span 4;
+            grid-row: 1;
+            text-align: center;
+        }
+
+        #question {
+            grid-row: 2;
+            grid-column: 2 / span 2;
+            border: solid black 1px;
+        }
+
+        #answer {
+            grid-row: 4;
+            grid-column: 2 / span 2;
+        }
+
+        #submit {
+            grid-row: 5;
+            grid-column: 2 / span 2;
+
+        }
+
+        #hiTracker {
+            grid-row: 3;
+            grid-column: 2;
+            border: solid black 1px;
+        }
+
+        #loTracker {
+            grid-row: 3;
+            grid-column: 3;
+            border: solid black 1px;
+        }
+    }
 </style>
