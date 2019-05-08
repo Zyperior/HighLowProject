c26ba524de7ff50db49a91b124d9d8ac48a8b9c1
filip <filip.christofferson@gmail.com>
2019-05-08 10:01:11 +0200

    Start work with game

---
+++ client/src/components/Game.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>
+
+        </p>
+        <input type="text" name="" placeholder="Enter your answer">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'game',
+        components,
+        methods: {
+            getQuestions() {
+                return this.$store.getters.getQuestions
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- client/src/store.js
+++ client/src/store.js
@@ -5,10 +5,54 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    questions: [{
+        id: 0,
+        question: 'What year was Öyvin born',
+        answer: 1984,
+        status: false
 
+      },
+      {
+        id: 1,
+        question: 'What year was Filip born',
+        answer: 1995,
+        status: false
+      },
+      {
+        id: 2,
+        question: 'How many players are there in a soccer game',
+        answer: 22,
+        status: false
+      }
+    ],
+
+    answeredQuestions: [{
+
+    }]
   },
+  getters: {
+    getQuestions: state => {
+
+      for (let i = 0; i < state.questions.length; i++) {
+        const element = state.questions[i];
+
+        return state.questions[i].question;
+      }
+
+    }
+  },
+
   mutations: {
 
+
+    questionAnswered: function (state, index) {
+      if (state.questions[i].status)
+        state.answeredQuestions.push(state.questions[i])
+    },
+    correctQuestion: function (state) {
+      if ("userInput" === state.question[0].answer)
+        state.question[0].status = true;
+    }
   },
   actions: {
 
