88e8dcecb66c4588b3c840c9adcabd893817e5e2
Andreas Albihn <albihn.andreas@gmail.com>
2019-05-08 10:20:34 +0200

    added missing export

--- server/api/model/question.js
+++ server/api/model/question.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 let questionShema = new Schema({
+    id: Number,
     question: String,
     answer: Number
 });
+
+module.exports = mongoose.model("Question", questionShema);
