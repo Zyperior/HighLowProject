81f1fff74da5ebc10426006120ece1acfc4cf834
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-05-09 14:43:40 +0100

    Make minor updates to Game.vue, in order to make the timer functionality work

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -2,21 +2,26 @@
     <div>
         <h1>Game Page</h1>
         <p>{{currentQuestion}}</p>
-        <div v-if="isRunning">
+        <div v-show="isRunning">            <!-- v-show är bättre än v-if??? -->
             <input v-model="answer" name="answer" placeholder="Enter your answer">
             <button @click="submitAnswer()">Submit Answer</button>
+
+            <Timer ref="myTimer"/>
         </div>
-        <button v-if="startStage" @click="startGame()">Start Game</button>
+        <button v-show="startStage" @click="startGame()">Start Game</button>        <!-- v-show är bättre än v-if??? -->
     </div>
 </template>
 <script>
+    import Timer from '@/components/Timer.vue'
+
     export default {
         methods: {
-            startGame() {
-            this.$store.commit('startGame');
+            startGame() {               
+                this.$store.commit('startGame');
+                this.$refs.myTimer.startTimer();
             },
             submitAnswer() {
-            this.$store.commit('submitAnswer');
+                this.$store.commit('submitAnswer');
             }
         },
         computed: {
@@ -37,6 +42,10 @@
             startStage() {
                 return this.$store.getters.getStartStage;
             }
+        },
+
+        components: {
+            Timer
         }
     }
 </script>
---
+++ client/src/components/Timer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button @click="startTimer">Start the timer</button>
+
+        <p>Time left: {{ timeLeft }}s</p>
+        
+        <p v-show="timeIsUp">Time's up!</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    methods: {
+
+        startTimer: function() {
+
+
+
+        }
+
+    }
+    
+}
+</script>
