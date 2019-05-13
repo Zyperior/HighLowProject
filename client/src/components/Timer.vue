6936b349253504251a16d6e348a6f07a17f6b257
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-05-13 13:59:53 +0100

    Rewrite the file Timer.vue to module form

--- client/src/components/Timer.vue
+++ client/src/components/Timer.vue
@@ -1,7 +1,5 @@
 <template>
     <div>
-        <!-- <button @click="startTimer">Start the timer</button> -->
-
         <p>Time left: {{ timeLeft }}s</p>
         
         <p v-show="timeIsUp">Time's up!</p>
@@ -12,39 +10,27 @@
 <script>
 export default {
 
-    data: function () {
+    // data: function () {
          
-        return {
-            time: 3000,
-            interval: 0
-        }
+    //     return {
+    //         time: 3000,
+    //         interval: 0
+    //     }
 
-    },
+    // },
 
     methods: {
 
         startTimer: function() {
 
-            var vm = this;
-
-            vm.$data.interval = setInterval(function() {
-
-                vm.$data.time -= 100;
-
-                if (vm.$data.time === 0) {
-
-                    clearInterval(vm.$data.interval);
-
-                }
-
-            }, 100);
+            this.$store.commit("startTimer");
 
         },
 
 
         stopTimer: function() {
 
-            clearInterval(this.$data.interval);
+            this.$store.commit("stopTimer");
 
         }
 
@@ -55,13 +41,13 @@ export default {
 
         timeLeft() {
 
-            return (this.time / 1000).toFixed(1);
+            return this.$store.getters.getTimeLeft;
 
         },
 
         timeIsUp() {
 
-            return this.time === 0;
+            return this.$store.getters.getTimeIsUp;
 
         }
     
