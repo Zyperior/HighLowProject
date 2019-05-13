1007ec08c93744f7ad56ffa33b85529d28777801
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-05-13 13:54:33 +0100

    Add timer to the modules in store.js

--- client/src/store.js
+++ client/src/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import game from './modules/game'
 import botLogic from './modules/storemodules'
 import generalStats from './modules/generalStats'
+import timer from './modules/timer'
 
 
 
@@ -13,6 +14,7 @@ export default new Vuex.Store({
     modules: {
         game,
         botLogic,
-        generalStats
+        generalStats,
+        timer
     }
 });
