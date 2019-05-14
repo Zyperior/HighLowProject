b97aab0813783dd2d81a3cac3125b7617318e165
Daniella Noren <daniellanoren@live.se>
2019-05-14 11:24:58 +0200

    Fix typo in button @click methods again

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -11,7 +11,7 @@
             <!--</div>-->
             <input v-model="answer" oninput="this.value=this.value.replace(/[^0-9]/g, '').replace(/^0/, '')" name="answer" placeholder="Enter your answer" :disabled="!playerTurn">
             <div>
-                <button @click="submitAnswer(answer); guess;" :disabled="!playerTurn">Submit Answer</button>
+                <button @click="submitAnswer(answer); guess();" :disabled="!playerTurn">Submit Answer</button>
                 <audio ref="audioTest" src="/testAudio.wav"></audio>
             </div>
             <Timer ref="myTimer"/>
