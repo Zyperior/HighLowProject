b0b5ce944948315125e9363486ec34a81e7d8bdd
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-05-14 10:57:13 +0100

    Register timer in store.js

--- client/src/store.js
+++ client/src/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import game from './modules/game'
 import botLogic from './modules/storemodules'
 import generalStats from './modules/generalStats'
+import timer from './modules/timer'
 
 
 
@@ -15,6 +16,7 @@ export default new Vuex.Store({
     modules: {
         game,
         botLogic,
-        generalStats
+        generalStats,
+        timer
     }
 });
