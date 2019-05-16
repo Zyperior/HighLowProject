2b2a50719eed4837ff11daa386651432d7be7d23
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-05-16 21:37:33 +0100

    Add css rules to the file App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,12 +15,33 @@
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  body {
+    margin: 0px;
+    background-color: honeydew;
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: 'Montserrat', Arial, Helvetica, sans-serif;
+    color: darkblue;
+    text-align: center;
+    font-size: 5vw;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  button {
+    font-family: 'Montserrat', Arial, Helvetica, sans-serif;
+    color: darkblue;
+    background-color: bisque;
+    width: 80%;
+    font-size: 5vw;
+    padding: 1em;
+    border-radius: 1em;
+    border-style: outset;
+    border-width: 8px;
+    border-color: beige;
+    margin: 1em 1em 0em 1em;
+  }
 </style>
