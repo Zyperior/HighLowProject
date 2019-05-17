70f759f7bbb6a69e303e95a26c4c8b1cc8ef1d67
filip <filip.christofferson@gmail.com>
2019-05-17 09:36:16 +0200

    Css stuff

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div id="app">
-    <NavBar/>
+    <NavBar class="topnav"/>
     <router-view/>
   </div>
 </template>
@@ -30,6 +30,47 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/* Style the navigation menu */
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+  position: relative;
+}
+
+/* Hide the links inside the navigation menu (except for logo/home) */
+.topnav #myLinks {
+  display: none;
+}
+
+/* Style navigation menu links */
+.topnav a {
+  color: white;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  display: block;
+}
+
+/* Style the hamburger menu */
+.topnav a.icon {
+  background: black;
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+/* Add a grey background color on mouse-over */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Style the active link (or home/logo) */
+.active {
+  background-color: #4CAF50;
+  color: white;
 }
 </style>
 
