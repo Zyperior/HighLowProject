f0f1a3b1a9ccee44d758af786a63a03841fba3db
Öyvind <oybo84@gmail.com>
2019-05-19 17:41:33 +0200

    Button for muting sound added. No styling

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -3,12 +3,18 @@
         <router-link to="/start">Home</router-link>
         <router-link to="about">About</router-link>
         <router-link to="/bots">Bots</router-link>
+        <button @click="muteSound()">Mute Sound</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: "NavBar"
+        name: "NavBar",
+        methods: {
+            muteSound() {
+                this.$store.commit('muteSound');
+            }
+        }
     }
 </script>
 
