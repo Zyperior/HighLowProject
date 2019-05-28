40eac15959dbf52342e85dd084cc72941e455cb7
Andreas Albihn <albihn.andreas@gmail.com>
2019-05-28 14:59:21 +0200

    added new component for the question

--- client/src/components/QuestionCard.vue
+++ client/src/components/QuestionCard.vue
@@ -1,7 +1,15 @@
 <template>
-    <div class="questionFrame">
-        <div class="questionText"></div>
-        <div class="questionPoints"></div>
+    <div class="wrapper">
+        <div class="questionGrid">
+            <div>Question:</div>
+            <div class="questionText"
+                 :class="{ smaller : currentQuestion.length > 90}">{{currentQuestion}}
+            </div>
+        </div>
+        <div class="pointsGrid">
+            <div>Points:</div>
+            <div>{{currentPoints}}</div>
+        </div>
     </div>
 </template>
 
@@ -13,12 +21,36 @@
                 return this.$store.getters.getCurrentQuestion;
             },
             currentPoints(){
-
+                return this.$store.getters.getCurrentQuestionValue;
             }
         }
     }
 </script>
 
 <style scoped>
+
+    .wrapper{
+        display: grid;
+        font-size: 15px;
+        border: solid rgb(205,226,203);
+    }
+
+    .questionGrid{
+        display: grid;
+        grid-template-columns: 75px auto;
+        min-height: 10vh;
+        text-align: start;
+    }
+
+    .smaller{
+        font-size: 14px;
+    }
+
+    .pointsGrid{
+        display: grid;
+        grid-template-columns: 75px auto;
+        text-align: start;
+        border-top: solid rgb(205,226,203);
+    }
 
 </style>
