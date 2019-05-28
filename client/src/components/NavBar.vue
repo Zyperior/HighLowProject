c81fa1216a21bc69955a05c8e5cd9b99e08a418e
IdaFagerlund <ida.fagerlund@hotmail.com>
2019-05-28 14:08:59 +0200

    Improved navigation bar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -7,33 +7,8 @@
             <div id="left-container">
                 <input id="search-field" v-model="username" placeholder="Search for user">
                 <img id="glass-icon" src="/soundfx/Magnifying_glass_icon.svg" @click="searchForUser(username)" />
-
-
-            </div>
-            <div id="links" v-if="activate === true">
-            <div @click="showMenu"> </div>
-            <div @click="showMenu"><router-link to="/">Home</router-link></div>
-            <div @click="showMenu"><router-link to="/rules">Rules</router-link></div>
-            <div @click="showMenu"><router-link to="/about">About</router-link></div>
-            <div @click="showMenu"><router-link to="/bots">Bots</router-link></div>
-            <div @click="showMenu"><router-link to="/settings">Settings</router-link></div>
-            <div @click="showMenu" v-show="displayExclusivePages.loggedInUser">
-            <router-link to="/profile">Profile</router-link>
-            </div>
-            <div @click="showMenu" v-show="displayExclusivePages.loggedInUser">
-            <router-link to="/suggest-question">Suggest a question</router-link>
-            </div>
-            <div @click="showMenu" v-show="displayExclusivePages.loggedInAdmin">
-            <router-link to="/admin">Admin page</router-link>
             </div>
 
-            <div @click="showMenu" v-show="!displayExclusivePages.loggedInUser">
-            <router-link to="/login">Login</router-link>
-            </div>
-            <div @click="showMenu" v-show="displayExclusivePages.loggedInUser">
-            <button id="logout-button" @click="logout">Logout button</button>
-            </div>
-            </div>
 
 
 
@@ -43,15 +18,42 @@
                     <!--<mute-sound-button id="sound-icon"/>-->
                 <!--</div>-->
 
-                <button id="login-button">Login</button>
+                <button>Login</button>
+                <button>Register</button>
 
 
-                <img @click="showMenu" id="menu-icon" src="https://www.lisastone.com/wp-content/uploads/pp/images/1494960420-menu.png"/>
+                <img @click="showMenu" id="menu-icon" src="../../public/menu.png">
                 <!--<div>-->
                     <!--&lt;!&ndash;<a class="menu-icon" href="javascript:void(0);" @click="showMenu"><i class="fa fa-bars"></i></a>&ndash;&gt;-->
                 <!--</div>-->
             </div>
 
+        </div>
+
+
+        <div id="links" v-if="activate === true">
+<!--            <div @click="showMenu"> </div>-->
+            <div @click="showMenu"><router-link to="/">Home</router-link></div>
+            <div @click="showMenu"><router-link to="/rules">Rules</router-link></div>
+            <div @click="showMenu"><router-link to="/about">About</router-link></div>
+            <div @click="showMenu"><router-link to="/bots">Bots</router-link></div>
+            <div @click="showMenu"><router-link to="/settings">Settings</router-link></div>
+            <div @click="showMenu" v-show="displayExclusivePages.loggedInUser">
+                <router-link to="/profile">Profile</router-link>
+            </div>
+            <div @click="showMenu" v-show="displayExclusivePages.loggedInUser">
+                <router-link to="/suggest-question">Suggest a question</router-link>
+            </div>
+            <div @click="showMenu" v-show="displayExclusivePages.loggedInAdmin">
+                <router-link to="/admin">Admin page</router-link>
+            </div>
+
+            <div @click="showMenu" v-show="!displayExclusivePages.loggedInUser">
+                <router-link to="/login">Login</router-link>
+            </div>
+            <div @click="showMenu" v-show="displayExclusivePages.loggedInUser">
+                <button id="logout-button" @click="logout">Logout button</button>
+            </div>
         </div>
 
 
@@ -125,7 +127,7 @@
         align-items: baseline;
 
         height: 3em;
-        background: #2c2c2c;
+        background: #393939;
 
 
     }
@@ -139,16 +141,16 @@
         align-items: center;
     }
 
-    #login-button{
+    #right-container button{
         border: none;
-        background: #7dd556;
+        background: #83cd62;
         color: white;
         border-radius: 5px;
         padding: 0.5em 1em;
         margin-left: 1em;
     }
 
-    #login-button:hover, #menu-icon:hover, #glass-icon{
+    #right-container button:hover, #menu-icon:hover, #glass-icon, #links{
         cursor: pointer;
     }
 
@@ -166,16 +168,25 @@
         margin-left: 1em;
     }
 
-    #links{
-        background: #7dd556;
 
+
+    #links{
+        background: gray;
+        width: 10em;
+        position: fixed;
+        right: 0;
+        margin-left: auto;
+    }
+    #links > div{
+        padding: 0.5em;
     }
     #links a{
         color: white;
         text-decoration: none;
+        display: block;
     }
-    #links a:hover{
-        background: blue;
+    #links > div:hover{
+        background: #555555;
     }
 
 
