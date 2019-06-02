3111d9a9d6d62837854ace55329c4b314ff8b91a
Öyvind <oybo84@gmail.com>
2019-06-02 10:06:41 +0200

    added two tests for Rules component

--- client/tests/unit/Rules.test.js
+++ client/tests/unit/Rules.test.js
@@ -2,8 +2,15 @@ import { mount } from '@vue/test-utils'
 import Rules from '../../src/views/Rules.vue'
 
 describe('Rules', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Rules)
+  const wrapper = mount(Rules)
+  it('is a Vue instance', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
+  it('renders the correct html', () => {
+    expect(wrapper.html()).toContain('<div class="title">Rules for High/Low</div>')
+  })
+  it('has not button', () => {
+    expect(wrapper.contains('button')).toBe(false)
+  })
+
 })
